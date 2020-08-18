4aa76439bd3de1ab6101ee900a7cde5e60c9ba44
JeongDaHyeon <48541114+JeongDaHyeon@users.noreply.github.com>
2020-08-18 10:40:21 +0900

    Get and Print the lists of particular directory

--- test_codes/print_file_list.py
+++ test_codes/print_file_list.py
@@ -4,6 +4,7 @@ import os
 directory_path = 'data'
 
 if __name__ == '__main__':
+    # parameter: path of directory
     file_list = os.listdir(directory_path)
     for fl in file_list:
         print(fl)
