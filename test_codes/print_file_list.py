3e7a35af98a30834bf097f7325de876ee1174431
JeongDaHyeon <48541114+JeongDaHyeon@users.noreply.github.com>
2020-08-18 10:38:07 +0900

    Create print_file_list.py

---
+++ test_codes/print_file_list.py
@@ -0,0 +1,9 @@
+import os
+
+# it will get file lists of directory whose path is directory_path
+directory_path = 'data'
+
+if __name__ == '__main__':
+    file_list = os.listdir(directory_path)
+    for fl in file_list:
+        print(fl)
